e6cad0b3e33e2fb481be1a993df2dfa94c737eb5
chenxuejin <13908434920@163.com>
2021-03-10 23:00:02 +0800

    'Button'

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button, {ButtonSize, ButtonType} from './components/Button/button'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Hello</Button>
+        <Button disabled={true}>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com'>Baidu Link</Button>
+        <Button btnType={ButtonType.Link} disabled={true}>Disabled Link</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
