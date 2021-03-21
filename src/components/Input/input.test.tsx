1d77cdd54c731b82c0d5fc5a1286403a4b925b51
projectcss <13908434920@163.com>
2021-03-21 22:49:29 +0800

    'input-test'

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, queryByText } from '@testing-library/react'
 
 import Input, { InputProps } from './input'
 
@@ -13,8 +13,25 @@ describe('test Input component', () => {
     const testNode = wrapper.getByPlaceholderText('test-input') as HTMLInputElement
     expect(testNode).toBeInTheDocument()
     expect(testNode).toHaveClass('viking-input-inner')
-    fireEvent.change(testNode, { target: { value: '23' } })
-    expect(defaultProps.onChange).toHaveBeenCalled()
+    fireEvent.change(testNode,{ target: { value: '23' } })
+    expect(defaultProps.onChange).toHaveBeenCalled();
     expect(testNode.value).toEqual('23')
+  })
+  it("should render the disabled Input on disabled property", () => {
+    const wrapper = render(<Input disabled placeholder="test-input"/>)
+    const testNode = wrapper.getByPlaceholderText("test-input") as HTMLInputElement;
+    expect(testNode.disabled).toBeTruthy();
+  })
+  it("should render different input sizes on size property", () => {
+    const wrapper = render(<Input placeholder="sizes" size="lg"/>)
+    const testCntainer = wrapper.container.querySelector(".viking-input-wrapper")
+    expect(testCntainer).toHaveClass("input-size-lg")
+  })
+  it("should render prepand abd append element on prepend/append property", () => {
+    const wrapper = render(<Input placeholder="pend" prepend="https://" append=".com"/>)
+    const testContainer = wrapper.container.querySelector('.viking-input-wrapper');
+    expect(testContainer).toHaveClass('input-group input-group-append input-group-prepend')
+    expect(wrapper.queryByText("https://")).toBeInTheDocument();
+    expect(wrapper.queryByText('.com')).toBeInTheDocument();
   })
 })
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -80,7 +80,7 @@ const SubMenu: React.FC<SubMenuProps> = ({index, title, children, className}) =>
         <li key={index} className={classes} {...hoverEvent}>
             <div className={'submenu-title'} {...clickEvents}>
                 {title}
-                <Icon icon="angle-down" className="arrow-icon"/>
+                {/* <Icon icon="angle-down" className="arrow-icon"/> */}
             </div>
             {renderChildren()}
         </li>
